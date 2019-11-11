bb4fd7e37d29aecd6f24ee9e02fbd7180722d9ea
Antsax <antwan.hommy@gmail.com>
2019-11-11 12:33:30 +0200

    checkstyle error testing

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -28,7 +28,8 @@ public class Varasto {
     }
 
     // --- ottavat aksessorit eli getterit: ---
-    public double getSaldo() {
+    public double getSaldo() 
+    {
         return saldo;
     }
 
